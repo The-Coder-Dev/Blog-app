1f2ac1090f6d0b6f47dd8d9a96f24ef8ff328593
Dev Sharma <devsharma2057@gmail.com>
2026-03-30 22:46:40 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
+  weight: ['400', '500', '600', '700', '800']
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.className} antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
